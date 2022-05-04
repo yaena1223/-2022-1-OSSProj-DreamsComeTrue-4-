c72f21f5c728b3a5e64c05a8c79475db7c0a8bd4
yaena1223 <yaena1223@dgu.ac.kr>
2022-05-04 15:19:46 +0900

    0504_modify_login(hash X)

--- data/database_user.py
+++ data/database_user.py
@@ -25,13 +25,16 @@ class Database:
             return True
 
     def match_idpw(self, id, pw): #아이디와 비번이 일치하는지 비교
-        input_password = pw.encode('utf-8') #입력번호 비밀번호 인코딩(문자 -> byte값으로)
+        #input_password = pw.encode('utf-8') #입력번호 비밀번호 인코딩(문자 -> byte값으로)
         curs = self.score_db.cursor(pymysql.cursors.DictCursor)
         sql = "SELECT * FROM users WHERE user_id=%s" 
         curs.execute(sql,id) #입력받은 id 서버로 전송
         data = curs.fetchone()  #입력받은 id와 일치하는 행 하나 선택
         curs.close()
-        check_password=bcrypt.checkpw(input_password,data['user_password'].encode('utf-8')) #입력한 비밀번호와 데이터베이스에 저장된 비밀번호 비교
+        #check_password=bcrypt.checkpw(input_password,data['user_password'].encode('utf-8')) #입력한 비밀번호와 데이터베이스에 저장된 비밀번호 비교
+        check_password= False
+        if pw == data['user_password']:
+            check_password = True
         return check_password
 
     def add_id(self, user_id): #아이디 추가
@@ -45,11 +48,15 @@ class Database:
     def add_pw(self, user_pw, user_id): #비밀번호 & coin 초기값 추가 * 캐릭터 초기값은 1로(캐릭터 숫자로 표현)
         initial_coin = 0 #가입시, 보유한 coin 0으로 설정
         initial_character = 1
-        new_pw = user_pw.encode('utf-8') #문자열을 byte값으로
+        '''new_pw = user_pw.encode('utf-8') #문자열을 byte값으로
         hashed_pw = bcrypt.hashpw(new_pw, bcrypt.gensalt()) #해싱
         curs = self.score_db.cursor()
         sql = "UPDATE users SET user_password=%s WHERE user_id=%s"
         curs.execute(sql, (hashed_pw, user_id)) #암호화된 비밀번호로 저장
+        self.score_db.commit()'''
+        curs = self.score_db.cursor()
+        sql = "UPDATE users SET user_password=%s WHERE user_id=%s"
+        curs.execute(sql,(user_pw, user_id))
         self.score_db.commit()
         curs = self.score_db.cursor()
         sql = "UPDATE users SET user_coin=%s WHERE user_id=%s"
