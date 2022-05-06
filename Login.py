c307d4290321a0788083f55da2b17d9ae81f30ca
yaena1223 <yaena1223@dgu.ac.kr>
2022-05-07 04:13:51 +0900

    0507 modify login ui

--- Login.py
+++ Login.py
@@ -27,18 +27,7 @@ screen = pygame.display.set_mode(size,pygame.RESIZABLE) #창크기 조정 가능
 ww, wh= pygame.display.get_surface().get_size() 
 Default.game.value["size"]["x"] = size[0] #Default는 Defs.py에 선언되어 있는 클래스명
 Default.game.value["size"]["y"] = size[1]
-menu_image = pygame_menu.baseimage.BaseImage(image_path=Images.login.value,drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL) #메뉴 이미지, Images는 Defs.py에 선언되어 있는 클래스명
-mytheme = pygame_menu.themes.THEME_BLUE # theme 종류 참고 : https://pygame-menu.readthedocs.io/en/3.5.2/_source/themes.html
-mytheme.background_color = menu_image 
-mytheme.widget_background_color = (150, 213, 252) #버튼 가독성 올리기 위해서 버튼 배경색 설정 : 하늘색
-menu = pygame_menu.Menu('DreamsComeTrue', ww,wh,theme=mytheme) #상단바 팀 이름
 
-background = pygame.image.load(Images.start.value) #기존 코드에 있었는데 필요한지 몰겠음!
-base_font = pygame.font.Font(None,32) #기본 폰트
-
-#user_id = '' #아이디 초기값
-#input_rect = pygame.Rect(200,200,140,32) #아이디 입력 창(사각형)
-#color = pygame.Color('lightskyblue3') 
 class Login:
     def __init__(self):
         self.id = ''
@@ -46,27 +35,47 @@ class Login:
         self.database = Database()
         self.coin = 0
         self.char = 1
+        menu_image = pygame_menu.baseimage.BaseImage(image_path=Images.login.value,drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL) #메뉴 이미지, Images는 Defs.py에 선언되어 있는 클래스명
+        mytheme = pygame_menu.Theme(
+            widget_font = pygame_menu.font.FONT_8BIT,
+            widget_background_color = (150, 213, 252), #버튼 가독성 올리기 위해서 버튼 배경색 설정 : 하늘색
+            title_font = pygame_menu.font.FONT_BEBAS,
+            selection_color = (0,0,0), #선택됐을때 글씨색 설정
+            widget_font_color = (255,255,255), #글씨색 설정
+            title_background_color = (0,100,162),
+            title_font_color = (255,255,255),
+            title_bar_style = pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
+        )
+        mytheme.background_color = menu_image
+        self.menu = pygame_menu.Menu('DreamsComeTrue', ww,wh,theme=mytheme) #상단바 
 
 
     def first_page(self): # 첫화면 
-        menu.clear()
-        menu.add.button(' Sign Up ', self.show_signup)
-        menu.add.vertical_margin(10)
-        menu.add.button('   Login   ', self.login_page)
-        menu.add.vertical_margin(10)
-        menu.add.button('2 Players', self.pvp_page)
-        menu.add.vertical_margin(10)
-        menu.add.button('    Quit    ', pygame_menu.events.EXIT)
+        self.menu.clear()
+        b1 = self.menu.add.button(' Sign Up ', self.show_signup)
+        self.menu.add.vertical_margin(10)
+        b2= self.menu.add.button('   Login   ', self.login_page)
+        self.menu.add.vertical_margin(10)
+        b3 = self.menu.add.button('2 Players', self.pvp_page)
+        self.menu.add.vertical_margin(10)
+        b4 = self.menu.add.button('    Quit    ', pygame_menu.events.EXIT)
+        button = [b1,b2,b3,b4]
+        for i in button:
+            i.resize(283, 38)
 
     def login_page(self): ##로그인 페이지
-        menu.clear()
-        mytheme.widget_background_color = (0,0,0,0) #투명 배경
-        menu.add.text_input('ID : ', maxchar=100, onreturn=self.get_id)
-        menu.add.text_input('PASSWORD : ', maxchar=100, onreturn=self.get_pw,password=True, password_char='*')
-        menu.add.button('  Log In   ', self.login)
-        menu.add.button('  back  ', self.first_page)
-        menu.add.button('        Quit         ', pygame_menu.events.EXIT)
-        mytheme.widget_background_color = (150, 213, 252)
+        self.menu.clear()
+        #mytheme.widget_background_color = (0,0,0,0) #투명 배경
+        self.menu.add.text_input('ID : ', maxchar=100, onreturn=self.get_id)
+        self.menu.add.text_input('PASSWORD : ', maxchar=100, onreturn=self.get_pw,password=True, password_char='*')
+        b1 = self.menu.add.button('  Login  ', self.login)
+        b2 = self.menu.add.button('  Back  ', self.first_page)
+        b3 = self.menu.add.button('  Quit  ', pygame_menu.events.EXIT)
+        button = [b1,b2,b3]
+        for i in button:
+            i.resize(200, 38)
+        
+        #mytheme.widget_background_color = (150, 213, 252)
 
     def login(self):
         if self.id:
@@ -98,19 +107,19 @@ class Login:
             self.login_page()
 
     def password_fail(self):
-        menu.clear()
-        menu.add.vertical_margin(10)
-        menu.add.label("   ID or Password Incorrect     ", selectable=False)
-        menu.add.vertical_margin(10)
-        menu.add.button('  back  ', self.login_page)
+        self.menu.clear()
+        self.menu.add.vertical_margin(10)
+        self.menu.add.label("   ID or Password Incorrect     ", selectable=False)
+        self.menu.add.vertical_margin(10)
+        self.menu.add.button('  back  ', self.login_page)
 
 
     def login_fail(self):
-        menu.clear()
-        menu.add.vertical_margin(10)
-        menu.add.label("   ID or Password Incorrect     ", selectable=False)
-        menu.add.vertical_margin(10)
-        menu.add.button('  back  ', self.login_page)
+        self.menu.clear()
+        self.menu.add.vertical_margin(10)
+        self.menu.add.label("   ID or Password Incorrect     ", selectable=False)
+        self.menu.add.vertical_margin(10)
+        self.menu.add.button('  back  ', self.login_page)
 
     #아이디 input값으로 변경
     def get_id(self,value):
@@ -133,28 +142,28 @@ class Login:
         self.database.add_pw(self.password,self.id)
 
     def show_signup(self):
-        menu.clear()
-        mytheme.widget_background_color = (0,0,0,0) #투명 배경
-        menu.add.text_input('ID : ', maxchar=15, onreturn=self.save_id) #현재의 문제점 : enter를 눌러야만 저장됨.
-        menu.add.text_input('PASSWORD : ', maxchar=50, onreturn=self.save_password,password=True, password_char='*')
-        menu.add.button('  Sign Up  ', self.login_page)
-        menu.add.button('  back  ', self.first_page)
-        menu.add.button('  Quit   ', pygame_menu.events.EXIT)
-        mytheme.widget_background_color = (150, 213, 252)
+        self.menu.clear()
+        #mytheme.widget_background_color = (0,0,0,0) #투명 배경
+        self.menu.add.text_input('ID : ', maxchar=15, onreturn=self.save_id) #현재의 문제점 : enter를 눌러야만 저장됨.
+        self.menu.add.text_input('PASSWORD : ', maxchar=50, onreturn=self.save_password,password=True, password_char='*')
+        self.menu.add.button('  Sign Up  ', self.login_page)
+        self.menu.add.button('  back  ', self.first_page)
+        self.menu.add.button('  Quit   ', pygame_menu.events.EXIT)
+
 
     def login_success(self):
         Main(screen).show()
 
     def signup_fail(self):
-        menu.clear()
-        menu.add.vertical_margin(10)
-        menu.add.label("    ID Already Exists     ", selectable=False)
-        menu.add.vertical_margin(10)
-        menu.add.button('  back  ', self.show_signup)
+        self.menu.clear()
+        self.menu.add.vertical_margin(10)
+        self.menu.add.label("    ID Already Exists     ", selectable=False)
+        self.menu.add.vertical_margin(10)
+        self.menu.add.button('  back  ', self.show_signup)
 
     def pvp_page(self): #2인 플레이어
-        menu.clear()
-        menu.add.button('  back  ', self.first_page)
+        self.menu.clear()
+        self.menu.add.button('  back  ', self.first_page)
 
 login = Login()
 login.first_page()   
@@ -181,16 +190,16 @@ if __name__ == '__main__':
             screen = pygame.display.set_mode(ratio_screen_size,pygame.RESIZABLE)
             window_size = screen.get_size()
             new_w, new_h = 1 * window_size[0], 1 * window_size[1]
-            menu.resize(new_w, new_h)
+            login.menu.resize(new_w, new_h)
             size = window_size
-            print(f'New menu size: {menu.get_size()}')
+            print(f'New menu size: {login.menu.get_size()}')
              
 
         # 화면에 메뉴 그리기
         screen.fill((25, 0, 50)) #값 변경해보고 지워봤는데 큰 변화 없음. 없어도 되는 기능인듯?
 
-        menu.update(events)
-        menu.draw(screen)
+        login.menu.update(events)
+        login.menu.draw(screen)
         #pygame.draw.rect(screen,color,input_rect,2)
 
 
